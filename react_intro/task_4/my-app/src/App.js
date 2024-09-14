2fb58018824fae116f01525b2a8ebb83f48970b4
Erica-Livia <e.ingabire1@alustudent.com>
2024-09-14 23:23:14 +0200

    intro t4

--- react_intro/task_4/my-app/src/App.js
+++ react_intro/task_4/my-app/src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import logo from './holberton-logo.jpg';
 import './App.css';
+import { getFullYear, getFooterCopy } from './utils';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-header">
+        <img src={logo} alt="Holberton Logo: Red Seahorse"/>
+        <h1>School dashboard</h1>
+      </div>
+        <div className="App-body">
+        <p>Login to access the full dashboard</p>
+        <div className="form">
+          <label htmlFor="email">
+            <span>Email:</span>
+            <input type="email" name="email" id="email" />
+          </label>
+
+          <label htmlFor="password">
+            <span>Password:</span>
+            <input type="password" name="password" id="pwd" />
+          </label>
+
+          <button onClick={() => {}}>OK</button>
+        </div>
+      </div>
+        <div className="App-footer">
+        <p>Copyright {getFullYear()} - {getFooterCopy(true)}</p>
+      </div>
     </div>
   );
 }
